71a9efc3cffc45d91f724ecd3002cb71317445df
Yashashwini Cheekuri <yashashwini.cheekuri@valuemomentum.com>
2022-06-23 17:53:55 +0530

    commitiing my java8 workspace

---
+++ Yashashwini/advjava1/Specificvalue.java
@@ -0,0 +1,23 @@
+package advjava1;
+
+public class Specificvalue {
+
+	public static void main(String[] args) {
+		int[] num= {1,2,3,4,5};
+		int tofind=4;
+		boolean found=false;
+		for(int n:num)
+		{
+			if(n==tofind)
+			{
+				found=true;
+				break;
+			}
+		}
+		if(found)
+			System.out.println(tofind+"is found");
+		else
+			System.out.println(tofind+"is not found");
+	}
+
+}
---
+++ Yashashwini/advjava1/Substring.java
@@ -0,0 +1,9 @@
+package Substring;
+
+public class Substring {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="valuemomentum";
+		System.out.println(s1.substring(6,11));
+		}
+}
