2185fd7017486e2a307b95c1a5cc65614d87a7a1
Kiranmai Malagaveli <kiranmai.malagaveli@valuemomentum.com>
2022-06-24 19:46:11 +0530

    commiting my java8 workspace

---
+++ kiranmai/java8/src/Indexofanarrayelement.java
@@ -0,0 +1,19 @@
+public class Indexofanarrayelement {
+
+
+
+public static void main(String[] args) {
+int[]a=new int[] {10,20,30,40};
+int num=40;
+for(int i=0;i<a.length;i++)
+{
+if(a[i]==num)
+{
+System.out.println(i);
+}
+}
+}
+
+
+
+}
---
+++ kiranmai/java8/src/Substring.java
@@ -0,0 +1,6 @@
+public class Substring {
+public static void main(String[] args) {
+String s1="valuemomentum";
+System.out.println(s1.substring(6,11));
+}
+}
