80d31324755caf4e84e082a5695bc7b1ee5914af
Rohith Kammili <rohith.kammili@valuemomentum.com>
2022-06-27 10:18:17 +0530

    commiting my workspace

---
+++ 7884/Asd32/src/Asd32.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Asd32 {
+
+	public static void main(String[] args) {
+		LocalDateTime now=LocalDateTime.now();
+		String format =now.format(DateTimeFormatter.ofPattern("MM/d  (ss)"));
+		System.out.println(format);
+
+	}
+
+}
