eaf2e48e758c6a53673dedc087a36387326eb1e3
Deekshitha Kishtamgari <deekshitha.kishtamgari@valuemomentum.com>
2022-06-27 14:39:06 +0530

    iam commiting my work space

---
+++ deekshitha/technical3/Assignment21/Department.java
@@ -0,0 +1,34 @@
+package Assignment21;
+public class Department {
+private String departmentName;
+private Integer noOfEmployees;
+public Department(String departmentName, Integer noOfEmployees) 
+{
+    super();
+    this.departmentName=departmentName;
+    this.noOfEmployees=noOfEmployees;
+}
+    public Department() 
+    { 
+
+    }
+    public String getDepartmentName() 
+    { 
+        return departmentName; 
+    }
+
+    public void setDepartmentName(String departmentName) 
+    { 
+        this.departmentName =departmentName; 
+    }
+
+    public Integer getNoOfEmployees() 
+    { 
+        return noOfEmployees; 
+    }
+
+    public void setNoOfEmployees(Integer noOfEmployees) 
+    { 
+        this.noOfEmployees = noOfEmployees; 
+    }
+}
