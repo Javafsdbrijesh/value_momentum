2fe7756dd5819c0239eda558eeecd0ef3e8fac44
Kiranmai Malagaveli <kiranmai.malagaveli@valuemomentum.com>
2022-06-27 19:31:30 +0530

    im commiting my workspace

---
+++ kiranmai/java8/src/Assignment33.java
@@ -0,0 +1,10 @@
+import java.time.ZonedDateTime;
+public class Assignment33 {
+
+
+
+public static void main(String[] args) {
+ZonedDateTime zdt = ZonedDateTime.now();
+System.out.println("The Zoned date time is: " + zdt);
+}
+}
