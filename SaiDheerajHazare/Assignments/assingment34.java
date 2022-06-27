0b13cce4eb198ae3f362d69f6974d208545aab08
Sai Dheeraj Hazare <saidheeraj.hazare@valuemomentum.com>
2022-06-27 13:25:20 +0530

    I am committing my workspace

---
+++ SaiDheerajHazare/Assignments/assingment34.java
@@ -0,0 +1,12 @@
+import java.time.*;
+public class assingment34 {
+	public static void main(String[] args)
+	{
+	// create a clock
+	ZoneId zid = ZoneId.of("Europe/Paris");
+	ZonedDateTime lt
+	= ZonedDateTime.now(zid);
+	System.out.println("ZonedDateTime : " + lt);
+	}
+	}
+
