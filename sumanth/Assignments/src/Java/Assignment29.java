66f6c0d12e9c0bab11432d340e6b49815333533d
Sumanth Venkata Bethanapalli <sumanth.bethanapalli@valuemomentum.com>
2022-06-28 22:28:19 +0530

    commiting my work space

---
+++ sumanth/Assignments/src/Java/Assignment29.java
@@ -0,0 +1,18 @@
+package Java;
+import java.time.LocalDateTime;
+
+
+
+public class Assignment29 {
+
+public static void main(String[] args) {
+
+LocalDateTime dateTime = LocalDateTime.now();
+
+//for Current time and date
+
+System.out.println("The local date and time is: " + dateTime);
+
+}
+
+}
---
+++ sumanth/microservices/Valuemomentum/Valuemomentum/src/test/java/com/javafsd/Valuemomentum/ValuemomentumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javafsd.Valuemomentum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValuemomentumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
