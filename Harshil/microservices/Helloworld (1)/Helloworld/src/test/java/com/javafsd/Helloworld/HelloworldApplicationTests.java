1281c2d547ebbfea5cfc924474abdd84c1a8ca5e
Harshil Parlikar <harshil.parlikar@valuemomentum.com>
2022-06-28 17:03:32 +0530

    commiting my workspace

---
+++ Harshil/microservices/Helloworld (1)/Helloworld/src/test/java/com/javafsd/Helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javafsd.Helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
