d125fdfb1dc057c6bbc8ca3830ff98d06615dd7f
Deekshitha Kishtamgari <deekshitha.kishtamgari@valuemomentum.com>
2022-06-28 22:17:16 +0530

    hibernate example

---
+++ deekshitha/hibernate-helloworld-example/src/main/java/net/javafsd/hibernate/util/HibernateUtility.java
@@ -0,0 +1,43 @@
+package net.javafsd.hibernate.util;
+ 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+ 
+public class HibernateUtility {
+
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if(sessionFactory == null) {
+            try {
+                //create registry
+                registry = new StandardServiceRegistryBuilder().configure().build();
+
+                //create MetadataSources
+                MetadataSources source = new MetadataSources(registry);
+
+                //create Metadata
+                Metadata metadata = source.getMetadataBuilder().build();
+
+                //Create SessionFactory
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+            } catch(Exception e) {
+                e.printStackTrace();
+                if(registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
+        }
+        return sessionFactory;
+    }
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
