4a6a24d2fb9e612a26544c223863891e17d03ad9
Karthik K Ayyalasomayajula <karthik.ayyalasomayajula@valuemomentum.com>
2022-06-29 23:37:22 +0530

    my workspace

---
+++ KarthikAK/Microservices/department-service/src/main/java/com/javafsd/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,25 @@
+package com.javafsd.departmentservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javafsd.departmentservice.entity.Department;
+import com.javafsd.departmentservice.service.DepartmentService;
+
+@RestController
+@RequestMapping("/departments")
+public class DepartmentController 
+{
+	@Autowired
+	private DepartmentService departmentService;
+	
+	@PostMapping("/values")
+	public Department saveDepartment(@RequestBody Department department)
+	{
+		Department departmentResponse = departmentService.saveDepartment(department);
+		return departmentResponse;
+	}
+}
