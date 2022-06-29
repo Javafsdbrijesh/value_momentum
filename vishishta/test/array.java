e9caa6d33692c49968e08d986cd94907d342b99e
Vishishta Kodumagulla <vishishta.kodumagulla@valuemomentum.com>
2022-06-29 11:04:45 +0530

    I committed my work

---
+++ vishishta/test/array.java
@@ -0,0 +1,18 @@
+package test;
+
+public class array {
+
+	public static void main(String[] args) {
+		int[] num = new int[5];
+		num[0]=2;
+		num[1]=3;
+		num[2]=4;
+		num[3]=5;
+		num[4]=6;
+		System.out.println("Array num[0]:"+ num[0]);
+		System.out.println("Array num[1]:"+ num[1]);
+		System.out.println("Array num[2]:"+ num[2]);
+		System.out.println("Array num[3]:"+ num[3]);
+		System.out.println("Array num[4]:"+ num[4]);
+		}
+}
