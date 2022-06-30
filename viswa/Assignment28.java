b806722acb57bccfaea257307e80b9b4c4e3f360
Viswanadh Naidu Kimidi <viswanadh.kimidi@valuemomentum.com>
2022-06-30 13:48:48 +0530

    im commting my work space

--- viswa/Assignment28.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalTime;
-public class Assignment28 {
-	  public static void main(String[] args) {
-	        
-	        LocalTime time = LocalTime.now();
-	        System.out.println(time);
-	    }
-
-}
--- viswa/Assignment31.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-public class Assignment31 {
-	 public static void main(String[] args) {
-	        
-	        LocalDate republicDate = LocalDate.of(1950, 1, 26);
-	        System.out.println(republicDate);         
-	    }
-
-}
