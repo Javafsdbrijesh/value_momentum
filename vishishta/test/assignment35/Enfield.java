75cb7a37a5c354552ac264c81e3c4e4b1f9e2c6f
Vishishta Kodumagulla <vishishta.kodumagulla@valuemomentum.com>
2022-07-04 19:36:24 +0530

    committed my files

---
+++ vishishta/test/assignment35/Enfield.java
@@ -0,0 +1,10 @@
+package assignment35;
+
+public class Enfield {
+
+	public void offRoading() {
+        System.out.println("Lets go to ladhak");
+
+	}
+
+}
---
+++ vishishta/test/assignment35/SportsBike.java
@@ -0,0 +1,10 @@
+package assignment35;
+
+public class SportsBike {
+
+	public void accelerate() {
+	     System.out.println("accelerating");
+
+	}
+
+}
