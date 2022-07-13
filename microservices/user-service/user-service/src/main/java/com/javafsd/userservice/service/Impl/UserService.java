dd7328aef7071f89e7747fc5a769eb67af1e8a6b
Sesha Sai Gadhi <seshasai.gadhi@valuemomentum.com>
2022-07-13 15:20:44 +0530

    Iam adding my assignments

---
+++ microservices/user-service/user-service/src/main/java/com/javafsd/userservice/service/Impl/UserService.java
@@ -0,0 +1,25 @@
+package com.javafsd.userservice.service.Impl;
+
+import java.util.List;
+
+
+import com.javafsd.userservice.entity.User;
+import com.javafsd.userservice.error.UserNotFoundException;
+
+ 
+public interface UserService {
+
+    public User saveUser(User User);
+
+    public List<User> getUsers();
+
+    public User getUserById(Long UserId) throws UserNotFoundException;
+
+    public User getUserByName(String name);
+
+    public void deleteUserById(Long UserId);
+
+    public User updateUser(User User, Long UserId);
+
+}
+
---
+++ microservices/user-service/user-service/src/main/java/com/javafsd/userservice/service/UserServiceImpl.java
@@ -0,0 +1,83 @@
+package com.javafsd.userservice.service;
+import java.util.List;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javafsd.userservice.entity.User;
+import com.javafsd.userservice.error.UserNotFoundException;
+import com.javafsd.userservice.repository.UserRepository;
+import com.javafsd.userservice.service.Impl.UserService;
+
+ 
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired 
+    private UserRepository UserRepository;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Override
+    public User saveUser(User User) {
+        User UserResponse = UserRepository.save(User);
+        LOGGER.info("User saved successfully in database");
+        return UserResponse; 
+    }
+
+    @Override
+    public List<User> getUsers() {
+        List<User> UserList = UserRepository.findAll();
+        return UserList;
+    }
+
+    @Override
+    public User getUserById(Long UserId) throws UserNotFoundException {
+        Optional<User> User = UserRepository.findById(UserId);
+        if(!User.isPresent()) {
+            throw new UserNotFoundException("User not Available for User Id: " + UserId);
+        }
+        return User.get();
+    }
+
+    @Override
+    public User getUserByName(String name) {
+        User User = UserRepository.findByUserName(name);
+        return User;
+    }
+
+    @Override
+    public void deleteUserById(Long UserId) {
+        UserRepository.deleteById(UserId);
+    }
+
+    @Override
+    public User updateUser(User UserReq, Long UserId) {
+        User UserDB = UserRepository.findById(UserId).get();
+
+        if(Objects.nonNull(UserReq.getFirstName()) && 
+                !"".equalsIgnoreCase(UserReq.getFirstName())){
+            UserDB.setFirstName(UserReq.getFirstName());
+            LOGGER.info("User firstName updates successfully");
+        }
+        if(Objects.nonNull(UserReq.getLastName()) && 
+                !"".equalsIgnoreCase(UserReq.getLastName())) {
+            UserDB.setLastName(UserReq.getLastName());
+            LOGGER.info("User last updates successfully");
+        }
+        if(Objects.nonNull(UserReq.getEmail()) && 
+                !"".equalsIgnoreCase(UserReq.getEmail())) {
+            UserDB.setEmail(UserReq.getEmail());
+            LOGGER.info("User  email updated successfully");
+        }
+        User dept = UserRepository.save(UserDB);
+
+        return dept;
+    }
+}
+
