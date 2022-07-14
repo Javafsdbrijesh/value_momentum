b8a52cb6304307a8485f041b22118758d51036f4
Harshil Parlikar <harshil.parlikar@valuemomentum.com>
2022-07-14 12:34:54 +0530

    commiting my work space

---
+++ Harshil/microservices/department-service/department-service/src/test/java/com/javafsd/departmentservice/service/impl/DepartmentServiceTest.java
@@ -0,0 +1,14 @@
+package com.javafsd.departmentservice.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DepartmentServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
