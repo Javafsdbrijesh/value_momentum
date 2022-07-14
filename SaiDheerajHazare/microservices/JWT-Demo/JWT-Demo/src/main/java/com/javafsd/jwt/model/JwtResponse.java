66e9e2dc3a1db14deca49e01883512105715326d
Sai Dheeraj Hazare <saidheeraj.hazare@valuemomentum.com>
2022-07-14 18:04:22 +0530

    Committing workspace

---
+++ SaiDheerajHazare/microservices/JWT-Demo/JWT-Demo/src/main/java/com/javafsd/jwt/model/JwtResponse.java
@@ -0,0 +1,29 @@
+package com.javafsd.jwt.model;
+
+public class JwtResponse {
+	
+	private String jwtToken;
+
+	public String getJwtToken() {
+		return jwtToken;
+	}
+
+	public void setJwtToken(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+
+	public JwtResponse(String jwtToken) {
+		super();
+		this.jwtToken = jwtToken;
+	}
+
+	public JwtResponse() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "JwtResponse [jwtToken=" + jwtToken + "]";
+	}
+
+}
