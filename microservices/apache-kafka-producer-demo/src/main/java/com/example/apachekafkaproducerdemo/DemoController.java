2fc932b62026a07f591f01a43347e3c7dfacd95c
Yashwanth Dayakar Dommana <yashwanth.dommana@valuemomentum.com>
2022-07-18 18:51:44 +0530

    kafka

---
+++ microservices/apache-kafka-producer-demo/src/main/java/com/example/apachekafkaproducerdemo/DemoController.java
@@ -0,0 +1,34 @@
+package com.example.apachekafkaproducerdemo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class DemoController
+{
+	@Autowired
+    KafkaTemplate<String, Book> kafkaTemplate;
+
+    private static final String TOPIC = "javafsd";
+    //private static final String TOPIC2 = "TestTopic";
+
+//    @GetMapping("/publish/{message}")
+//    public String publishMessage(@PathVariable("message") final String message)
+//    {
+//        //kafkaTemplate.send(TOPIC, message);
+//        kafkaTemplate.send(TOPIC2, message);
+//        return "Message published successfully!";
+//    }
+
+    @PutMapping("/publish")
+    public String publishMessage(@RequestBody Book book)
+    {
+        //kafkaTemplate.send(TOPIC, message);
+        kafkaTemplate.send(TOPIC, book);
+        return "Message published successfully!";
+    }
+}
+
