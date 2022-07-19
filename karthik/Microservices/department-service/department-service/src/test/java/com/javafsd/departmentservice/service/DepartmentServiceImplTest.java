4e46aad2f7c7078c2bfa35363f9596fe9b33b90c
Karthik Kollipara <karthik.kollipara@valuemomentum.com>
2022-07-19 15:43:13 +0530

    update

---
+++ karthik/Microservices/department-service/department-service/src/test/java/com/javafsd/departmentservice/service/DepartmentServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.javafsd.departmentservice.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class DepartmentServiceImplTest {
+
+	
+
+}
