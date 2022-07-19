c370d4bfdf33b1de4ebf1f3367b417024fcfc050
Karthik Kollipara <karthik.kollipara@valuemomentum.com>
2022-07-19 15:47:24 +0530

    commiting swagger

---
+++ karthik/swagger-demo/swagger-demo/src/main/java/com/javafsd/swaggerdemo/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.javafsd.swaggerdemo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.javafsd.swaggerdemo.entity.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+	
+
+}
