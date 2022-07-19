5abc91b52c2e11844192b985ad3b47ef06757228
Harshavardhan Amula <harshavardhan.amula@valuemomentum.com>
2022-07-19 16:53:26 +0530

    Commting my workspace

---
+++ Harshavardhan/microservice/Swagger-demo/Swagger-demo/src/main/java/com/javafsd/Swaggerdemo/SwaggerDemoApplication.java
@@ -0,0 +1,18 @@
+package com.javafsd.Swaggerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableSwagger2
+public class SwaggerDemoApplication {
+
+ 
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerDemoApplication.class, args);
+    }
+
+ 
+
+}
---
+++ Harshavardhan/microservice/Swagger-demo/Swagger-demo/src/main/java/com/javafsd/Swaggerdemo/service/BookService.java
@@ -0,0 +1,27 @@
+package com.javafsd.Swaggerdemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.javafsd.Swaggerdemo.entity.Book;
+import com.javafsd.Swaggerdemo.repository.BookRepository;
+
+@Service
+public class BookService {
+ 
+	@Autowired
+	private BookRepository bookrepository;
+	
+	public Book saveBook(Book book) {
+		Book booksave = bookrepository.save(book);
+		        return booksave;
+	}
+	@GetMapping("/book/")
+	public Book getAllBooks() {
+		Book bookDb= (Book) bookrepository.findAll();
+		return bookDb;
+	}
+}
