d0ef114ed63b32ea91517a9c291beb92e95f402d
Saivineeth Dachepally <saivineeth.dachepally@valuemomentum.com>
2022-07-20 19:05:50 +0530

    commiting workspace

--- Vineeth/microservices/service-registry/service-registry/src/main/java/com/javafsd/serviceregistry/ServiceRegistryApplication.java
+++ Vineeth/microservices/service-registry/service-registry/src/main/java/com/javafsd/serviceregistry/ServiceRegistryApplication.java
@@ -1,7 +1,8 @@
 package com.javafsd.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
-
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
