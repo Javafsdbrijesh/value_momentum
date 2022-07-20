6f17c3ccdd4694ccb8bf4342fd59306a69be1b32
Deekshitha Kishtamgari <deekshitha.kishtamgari@valuemomentum.com>
2022-07-20 11:43:19 +0530

    commiting my workspace

---
+++ deekshitha/microservices/JWT-demo/src/main/java/com/javafsd/JWTdemo/model/JwtResponse.java
@@ -0,0 +1,20 @@
+package com.javafsd.JWTdemo.model;
+
+
+public class JwtResponse {
+	
+	private String jwtToken;
+
+	public String getJwtToken() {
+		return jwtToken;
+	}
+
+	public void setJwtToken(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+
+	public JwtResponse(String token) {
+		
+	}
+
+}
