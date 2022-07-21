79aee4c00526251823a03196103812a4bf3fb931
Kedhar Reddy Bussa <kedhar.bussa@valuemomentum.com>
2022-07-21 12:20:40 +0530

    MYSQL

--- kedhar/Mysql workspace/Assignment55-60.sql
+++ kedhar/Mysql workspace/Assignment55-60.sql
@@ -18,31 +18,38 @@ Customers
 where
 City = "Hyderabad";
 
+
 drop view Hyd_Customers;
-
 drop table Customers;
-
 drop database customer_database;
+
 
 alter table customers
 add column Gender varchar(25);
-
+ 
 alter table customers
 drop column Gender;
-
+ 
 alter table customers
 modify Phone_Number int;
-
+ 
 truncate table customers;
+ 
+insert into customers(Customer_ID, Customer_Name, Phone_Number, Address, City, Country)
+values (1, "Sai Dheeraj", 1285698, "Akhila Enclave", "Hyderbad","India");
 
 insert into customers(Customer_ID, Customer_Name, Phone_Number, Address, City, Country)
-values (2, "Dherubhai", 88978783, "panjim", "goa","India");
+values (2, "Rohit", 987634, "Beerumguda", "Goa","India");
 
+insert into customers(Customer_ID, Customer_Name, Phone_Number, Address, City, Country)
+values (3, "D Dheeraj", 7827783, "kukatpally", "Bihar","India");
+
+
+ 
 update customers
-set Address = "17 Nehru Road" where Customer_ID = 1;
-
+set Address = "17 Nehru Road" where customer_id = 1;
+ 
 select * from customers;
-
 select Customer_Name, City from customers;
-
-select * from customers where Customer_Name like "D%";
+ 
+select * from customers where Customer_Name like "d%";
