cdc5bc2838db7142f2edd69e86066a99a7d57430
javafsdbrijesh <javafsdbrijesh@gmail.com>
2022-07-21 10:58:15 +0530

    updating repository

--- Brijesh/microservices/department-service/src/main/java/com/javafsd/departmentservice/DepartmentServiceApplication.java
+++ Brijesh/microservices/department-service/src/main/java/com/javafsd/departmentservice/DepartmentServiceApplication.java
@@ -2,10 +2,11 @@ package com.javafsd.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableSwagger2
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
