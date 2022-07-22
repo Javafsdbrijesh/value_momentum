bad831edf08f957cec905e34dc3559faf9a0231b
Viswanadh Naidu Kimidi <viswanadh.kimidi@valuemomentum.com>
2022-07-22 16:25:02 +0530

    commiting my workspace

---
+++ viswa/frontend/assignment78.js
@@ -0,0 +1,15 @@
+function hello() {
+
+    alert("Hey guys I am alert!");
+
+    alert("Hey this is alert 2!");
+
+   
+
+    var p = document.getElementById("demo");
+
+   
+
+    p.style.color = "white";
+
+  }
--- viswa/microservice/JWT-Demo/src/main/java/com/javafsd/jwt/service/UserService.java
+++ viswa/microservice/JWT-Demo/src/main/java/com/javafsd/jwt/service/UserService.java
@@ -6,12 +6,16 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// Here we have to write logic to get user from database
+		
+		// Here we have to write logic to get user from database.
+		
 		return new User("admin","password",new ArrayList<>());
 	}
 
