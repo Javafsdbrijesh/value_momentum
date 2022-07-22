47e01891e2ae5edc5773178b5e753b8abf492709
Karthik K Ayyalasomayajula <karthik.ayyalasomayajula@valuemomentum.com>
2022-07-22 15:20:16 +0530

    front-end workspace

---
+++ KarthikAK/Front-end/test.js
@@ -0,0 +1,9 @@
+function welcome()
+{
+    alert("Welcome to HTML😉");
+    prompt("Please login with correct credentials🔑");
+    confirm("The login credentials are valid ✔");
+
+    var p = document.getElementById("demo");
+    p.style.color = "blue";
+}
