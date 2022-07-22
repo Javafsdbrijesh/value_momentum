475531cc3d6f2d61154f51c5b6d0515559d0a69a
Saivineeth Dachepally <saivineeth.dachepally@valuemomentum.com>
2022-07-22 14:19:37 +0530

    commiting workspace

---
+++ Vineeth/hibernate-helloworld-example-demo/hibernate-helloworld-example-demo/src/main/java/com/javafsd/hibernatehelloworldexampledemo/HibernateHelloworldExampleDemoApplication.java
@@ -0,0 +1,14 @@
+package com.javafsd.hibernatehelloworldexampledemo;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateHelloworldExampleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateHelloworldExampleDemoApplication.class, args);
+	}
+
+}
---
+++ Vineeth/hibernate-helloworld-example-demo/hibernate-helloworld-example-demo/src/test/java/com/javafsd/hibernatehelloworldexampledemo/HibernateHelloworldExampleDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.javafsd.hibernatehelloworldexampledemo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateHelloworldExampleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
