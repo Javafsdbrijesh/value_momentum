6aecc4d0514f0575ae95ec8ba11f0b3a235f80f0
Sai Vineeth Jaya Satya Kotamsetty <saivineeth.kotamsetty@valuemomentum.com>
2022-07-22 15:51:52 +0530

    commiting my workspace

---
+++ saivineeth/hibernate-helloworld-example/src/main/java/com/javafsd/hibernatehelloworldexample/HibernateHelloworldExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javafsd.hibernatehelloworldexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateHelloworldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateHelloworldExampleApplication.class, args);
+	}
+
+}
---
+++ saivineeth/hibernate-helloworld-example/src/test/java/com/javafsd/hibernatehelloworldexample/HibernateHelloworldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javafsd.hibernatehelloworldexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateHelloworldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
