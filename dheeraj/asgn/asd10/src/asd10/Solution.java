f07b314ef7c5c4bf5a42cb3c662b19f22c4238ea
saidheeraj30 <108113322+saidheeraj30@users.noreply.github.com>
2022-07-26 11:46:26 +0530

    Delete dheeraj directory

--- dheeraj/asgn/asd10/src/asd10/Solution.java
+++
@@ -1,9 +0,0 @@
-package asd10;
-public class Solution{
-public static void main(String args[]){
-Training training =new Training();
-((Training) training).getNumberOfParticipants();
-FullStackTraining fullStackTraining= new FullStackTraining();
-fullStackTraining.getNumberOfParticipants();
-}
-}
